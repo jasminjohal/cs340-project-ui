2354ac9044b6958157896ba039fe8e77b94990ea
KayCave <73556270+KayCave@users.noreply.github.com>
2020-11-01 17:05:00 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var addBookButton = document.getElementById('add-book-button');
+var addBookForm = document.getElementById('add-book-form');
+addBookButton.addEventListener('click', function() {
+    console.log('hi');
+    addBookForm.style.display = "block";
+})
